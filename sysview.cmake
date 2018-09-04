ba56faa66bebbd73688471d7416bb12a5737560d
Tobias Rothfelder <TRothfelder@users.noreply.github.com>
2018-09-04 21:09:32 +0200

    fixed some typos in cmakes

--- sysview.cmake
+++ sysview.cmake
@@ -4,8 +4,8 @@ add_library(sysview STATIC "")
 
 
 file(GLOB SYSVIEW_SRC
-    "${SYSVIEW_DIR}/*.c"
-    "src/segger/*.c"
+    ${SYSVIEW_DIR}/*.c
+    src/segger/*.c
 )
 
 target_sources(sysview
@@ -13,8 +13,8 @@ target_sources(sysview
         ${SYSVIEW_SRC}
 )
 
-target_include_directories(sysview PUBLIC "${SYSVIEW_DIR}")
-target_include_directories(sysview PUBLIC "inc/segger")
+target_include_directories(sysview PUBLIC ${SYSVIEW_DIR})
+target_include_directories(sysview PUBLIC inc/segger)
 add_definitions(-DSEGGER_SYSVIEW_USE)
 if(TARGET freertos)
   target_link_libraries(sysview freertos)
