80c5175279049940dc4567734f08017582e7472d
Tobias Rothfelder <TRothfelder@users.noreply.github.com>
2018-09-05 19:03:20 +0200

    added helper class for args of task

--- src/tasks/init.cpp
+++ src/tasks/init.cpp
@@ -13,8 +13,25 @@ extern "C" {
 
 #include "tasks/mytasks.hpp"
 
-uint16_t led1 = GPIO12;
-uint16_t led2 = GPIO14;
+const uint16_t led1io = GPIO12;
+const uint32_t led1Port = GPIOD;
+freertosArgs_t args1 = {
+	.count = 2,
+	.args = {
+		(void *) &led1Port,
+		(void *) &led1io,
+	},
+};
+
+const uint16_t led2io = GPIO14;
+const uint32_t led2Port = GPIOD;
+freertosArgs_t args2 = {
+	.count = 2,
+	.args = {
+		(void *) &led2Port,
+		(void *) &led2io,
+	},
+};
 
 void MyTasks::init( void *arg ) {
 	( void ) arg;
@@ -27,7 +44,6 @@ void MyTasks::init( void *arg ) {
 	gfxInit();
 	// Code Here
 	// Get the screen size
-
 	gdispSetOrientation(GDISP_ROTATE_270);
 	width = gdispGetWidth();
 	height = gdispGetHeight();
@@ -37,15 +53,14 @@ void MyTasks::init( void *arg ) {
 	for(i = 5, j = 0; i < width && j < height; i += 7, j += i/20)
 			gdispDrawPixel(i, j, White);
 
-
-	xTaskCreate(&MyTasks::ledblink,"LEDBLINK1",100,&led1,configMAX_PRIORITIES-1, &tasks.ledblinkTaskHandle1);
+	xTaskCreate(&MyTasks::ledblink,"LEDBLINK1",100,( void* )&args1,configMAX_PRIORITIES-1, &tasks.ledblinkTaskHandle1);
 	#if defined(SEGGER_SYSVIEW_USE)
 		SEGGER_SYSVIEW_Print("LEDBLINK1 task created.");
 	#endif
 
 	vTaskDelay(250 / portTICK_RATE_MS);
 
-	xTaskCreate(&MyTasks::ledblink,"LEDBLINK2",100,&led2,configMAX_PRIORITIES-1, &tasks.ledblinkTaskHandle2);
+	xTaskCreate(&MyTasks::ledblink,"LEDBLINK2",100,( void* )&args2,configMAX_PRIORITIES-1, &tasks.ledblinkTaskHandle2);
 	#if defined(SEGGER_SYSVIEW_USE)
 		SEGGER_SYSVIEW_Print("LEDBLINK2 task created.");
 	#endif
